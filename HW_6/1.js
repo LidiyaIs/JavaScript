3eed90db71b042d3d94d74b95c165893a41a1849
Isaeva Lidiya <isaeva.reid@gmail.ru>
2023-03-15 12:39:11 +0500

    ДЗ_6 корректировка

--- HW_6/1.js
+++ HW_6/1.js
@@ -15,10 +15,10 @@ console.log(elem);
 2. Внутри всех элементов на странице, которые имеют класс "card-link",
 поменяйте текст внутри элемента на "ссылка".
 */
-const elements = document.querySelectorAll('card-link');
-    elements.forEach(element => {
-        element.textContent = 'ссылка';
-    });
+const elements = document.querySelectorAll('.card-link');
+elements.forEach(element => {
+element.textContent = 'ссылка';
+});
 /*
 3. Найти все элементы на странице с классом "card-link", которые лежат в
 элементе с классом "card-body" и вывести полученную коллекцию в консоль.
@@ -28,7 +28,8 @@ console.log(elems);
 /*
 4. Найти первый попавшийся элемент на странице у которого есть атрибут
 data-number со значением 50 и вывести его в консоль.
-*/const element = document.querySelector('[data-number="50"]');
+*/
+const element = document.querySelector('[data-number="50"]');
 console.log(element);
 /*
 5. Выведите содержимое title страницы в консоль.
@@ -38,7 +39,7 @@ console.log(document.title);
 6. Получите элемент с классом "card-title" и выведите его родительский узел
 в консоль.
 */
-const pElem = document.querySelector('.card-title').parentNode;
+const pElem = document.querySelector('.card-title').parentNode; //.parentNode или .parentElement - одно и то же, можно как угодно получать родителя.
 console.log(pElem);
 /*
 7. Создайте тег `p`, запишите внутри него текст "Привет" и добавьте созданный
